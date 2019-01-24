fbcf070398ee13c955cab949dd4124d031fb8ca8
Raiicon <orankai@gmail.com>
2019-01-24 14:15:02 -0500

    added a 'Cabinet' component, a bottom nav bar for our board

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from './components/NavBar';
+import Cabinet from './components/Cabinet';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <NavBar />
-     
+      <Cabinet>
+
+      </Cabinet>
+      </div>
     );
   }
 }
---
+++ client/src/components/Cabinet/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Cabinet () {
+    return (
+        <nav class="navbar fixed-bottom navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">The Cabinet</a>
+        </nav>
+    )
+}
+
+export default Cabinet;
