b6fd52a7eddbc9a6b865164dd465a3fc25a0c996
Raiicon <orankai@gmail.com>
2019-01-24 23:01:26 -0500

    fixed errors, adding dropup menu for create

--- client/src/components/Cabinet/CreateMenu/index.js
+++ client/src/components/Cabinet/CreateMenu/index.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import './style.css';
 
+function CreateMenu({ }) {
+    return (
+
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Create
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Create New Tack</a>
+                <a class="dropdown-item" href="#">Create Tack From Template</a>
+                <a class="dropdown-item" href="#">Import Tack</a>
+            </div>
+        </div>
+    )
+}
+
+export default CreateMenu;
