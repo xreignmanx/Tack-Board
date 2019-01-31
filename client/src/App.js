d528bdccf1c74d04e5ba832e09225d30bd4c5dd7
xreignmanx <rtballentine@yahoo.com>
2019-01-30 23:13:21 -0500

    pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
 import NavBar from './components/NavBar';
-import Cabinet from './components/Cabinet';
-import TackBoard from './components/TackBoard';
+// Import Pages
+import Landing from "./pages/Landing";
+import Lobby from "./pages/Lobby";
+import Host from "./pages/Host";
+import Client from "./pages/Client";
 
-class App extends Component {
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-      <NavBar />
-      <TackBoard>
-
-        Shoop da whoop!
-      </TackBoard>
-
-      <Cabinet>
-
-      </Cabinet>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/Lobby" component={Lobby} />
+          <Route exact path="/Host" component={Host} />
+          <Route exact path="/Client" component={Client} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
+    
