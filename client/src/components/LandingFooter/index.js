2a801a7421424e51a8b965df4df37604d29bcf63
xreignmanx <rtballentine@yahoo.com>
2019-03-03 21:05:13 -0500

    landing page adj

--- client/src/components/LandingFooter/index.js
+++ client/src/components/LandingFooter/index.js
@@ -4,18 +4,15 @@ import './style.css';
 function LandingFooter() {
     return (
         <div className="landingFooter">
-            <div class="col-sm-4" style={{color: 'grey', borderRadius: 0,
-                    backgroundColor: '#6b2302', justifyContent: 'space-around', alignContent: 'center'}}>
-            <button type="submit" href="/login" style={{color: 'grey', borderRadius: 0,
-                    backgroundColor: '#6b2302', borderColor: 'brown', borderWidth: '5px', borderStyle: 'ridge',
+            <div class="col-sm-4">
+            <button type="submit" href="/login" style={{color: 'tan', borderRadius: 0,
+                    backgroundColor: "brown", borderColor: 'brown', borderWidth: '5px', borderStyle: 'ridge',
                     fontWeight: 'bold', justifyContent: 'center', alignContent: 'center'}}class="btn">LOGIN</button>
             </div>
             <div class="col-sm-4"></div>
-            <div class="col-sm-4" style={{color: 'grey', borderRadius: 0,
-                    backgroundColor: '#6b2302', borderColor: 'brown', borderWidth: '5px', borderStyle: 'ridge',
-                    fontWeight: 'bold', justifyContent: 'center', alignContent: 'center'}} >
-            <button type="submit" href="/register" style={{color: 'grey',  borderRadius: 0,
-                    backgroundColor: '#6b2302', borderColor: 'brown', borderWidth: '5px', borderStyle: 'ridge',
+            <div class="col-sm-4">
+            <button type="submit" href="/register" style={{color: 'tan',  borderRadius: 0,
+                    backgroundColor: 'brown', borderColor: 'brown', borderWidth: '5px', borderStyle: 'ridge',
                     fontWeight: 'bold'}}class="btn">REGISTER</button>
             </div>
 
